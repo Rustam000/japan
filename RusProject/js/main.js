74e1df6222db9668a8af3a493f20c6b20d776c03
Rustam000 <61691677+Rustam000@users.noreply.github.com>
2023-07-21 18:32:44 +0600

    japan
    japan

---
+++ RusProject/js/main.js
@@ -0,0 +1,10 @@
+(fanction(){
+	const header = document.querySelector('.header');
+	window.onscroll = () => {
+		if(window.pageYOffset > 50){
+			header.classList.add('header__active');
+		}else{
+			header.classList.remove('header__active');
+		}
+	};
+}());
